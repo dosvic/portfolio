eb5d0933d7d53639323f4d106a96cd6146068945
Deniz Osmanovic <47736428+DOsmanovic@users.noreply.github.com>
2025-12-28 18:47:22 +0100

    feat: improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://denizosmanovic.com',
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          de: 'de',
+        },
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
